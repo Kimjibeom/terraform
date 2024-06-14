9e45da1d88f6d2412043ef8436e594ed8f65fbd9
김지범 <kimjibeom@gimjibecBookAir.kornet>
2024-06-14 17:49:27 +0900

    Hello World

--- main/main.tf
+++ main/main.tf
@@ -1,3 +1,30 @@
 provider "aws" {
     region = "us-east-2"
 }
+
+resource "aws_instance" "terraform" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    tags = {
+        Name = "terraform"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-instance"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
