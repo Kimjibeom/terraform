768e3ea58c66f6a5fdbf6a2a0d3b2096e817db80
김지범 <kimjibeom@gimjibeom-ui-MacBookAir.local>
2024-06-19 17:02:31 +0900

    list_num

--- main/main.tf
+++ main/main.tf
@@ -40,3 +40,9 @@ variable "list_example" {
     type = list
     default = ["a", "b", "c"]
 }
+
+variable "list_numeric_example" {
+    description = "An example of a numeric list in Terraform"
+    type = list(number)
+    default = [1, 2, 3]
+}
