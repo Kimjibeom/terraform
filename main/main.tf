586670dfb292f6fecb048c13b913d93289ef19c8
Kimjibeom <a889997@naver.com>
2024-06-19 17:10:56 +0900

    object

--- main/main.tf
+++ main/main.tf
@@ -57,3 +57,19 @@ variable "map_example" {
         key3 = "value3"
     }
 }
+
+variable "object_example" {
+    description = "An example of a structural type in Terraform"
+    type = object({
+        name    = string
+        age     = number
+        tags    = list(string)
+        enabled = bool
+    })
+    default = {
+        name    = "value1"
+        age     = 42
+        tags    = ["a", "b", "c"]
+        enabled = true
+    }
+}
