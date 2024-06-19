97af78bf18044e2fe06d25f0f6890e66b79d32e2
김지범 <kimjibeom@gimjibeom-ui-MacBookAir.local>
2024-06-19 16:59:25 +0900

    list

--- main/main.tf
+++ main/main.tf
@@ -34,3 +34,9 @@ variable "number_example" {
     type = number
     default = 42
 }
+
+variable "list_example" {
+    description = "An example of a list in Terraform"
+    type = list
+    default = ["a", "b", "c"]
+}
