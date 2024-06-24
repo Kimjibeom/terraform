4e126c7a8a8db585d2662c5236d586a7ca0a509a
Kimjibeom <a889997@naver.com>
2024-06-24 16:00:59 +0900

    asg

--- main/main.tf
+++ main/main.tf
@@ -84,3 +84,28 @@ output "public_ip" {
     value = aws_instance.example.public_ip
     description = "The public IP address of the web server"
 }
+
+resource "aws_launch_configuration" "example" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                  = "Name"
+        value                = "terraform-asg-example"
+        propagate_at_launch  = true
+    }
+}
